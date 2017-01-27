65bd9d02ef3caf8f83cd4eec7decbc29dd401947
App Academy Student <student@appacademy.io>
2017-01-26 17:37:20 -0800

    add Board#place_bombs #place_numbers and helper methods

--- board.rb
+++ board.rb
@@ -1,6 +1,7 @@
 require_relative 'tile'
 
 class Board
+  attr_reader :size
   def initialize(size, num_of_bombs)
     @size = size
     @num_of_bombs = num_of_bombs
@@ -9,9 +10,42 @@ class Board
 
   def place_bombs
     until @grid.flatten.count(:bomb) == @num_of_bombs
-      pos = [rand(@size - 1), rand(@size - 1)]
+      pos = [rand(size - 1), rand(size - 1)]
       self[pos] = :bomb
     end
+  end
+
+  def place_numbers
+    (size).times do |row|
+      (size).times do |col|
+        pos = [row, col]
+        self[pos] = number_of_bombs(pos) unless self[pos] == :bomb
+      end
+    end
+  end
+
+  def number_of_bombs(pos)
+    adjacent_squares(pos).count { |square| self[square] == :bomb }
+  end
+
+  def adjacent_squares(pos)
+    row, col = pos
+    adjacents = []
+
+    (row - 1..row + 1).each do |row|
+      (col - 1..col + 1).each do |col|
+        next if [row, col] == pos
+        adjacents << [row, col] if valid_pos?([row, col])
+      end
+    end
+
+    adjacents
+  end
+
+  def valid_pos?(pos)
+    row, col = pos
+    (0...size).include?(row) &&
+      (0...size).include?(col)
   end
 
   def [](pos)
